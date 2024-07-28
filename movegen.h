c863f90e1f6b882121c888c3ae40ffb093ac956a
Ryan Baker <rbaker@xtier.com>
2024-07-27 23:16:05 -0400

    elided roughly 9/10 of missed stored i64's and 1/2 of failed eliminated loads

--- movegen.h
+++ movegen.h
@@ -36,49 +36,47 @@ private:
 }; // struct MoveList
 
 
-// holds and updates variables used for move generation
-namespace Movegen
+struct GenData
 {
-	static constinit Square ep_target {NO_SQUARE};
+	uint64_t ep_target = 0;
 
-	static constinit uint64_t rook_pins;
-	static constinit uint64_t bishop_pins;
+	uint64_t checkmask = Bitboard::BOARD;
+	uint64_t kingban = 0;
 
-	static constinit uint64_t checkmask;
-	static constinit uint64_t kingban;
+	uint64_t bishop_pins = 0;
+	uint64_t rook_pins = 0;
 
 	force_inline void register_slider_check(Square ksq, Square ssq);
 
 	template<Color Us, bool EnPassant>
 	force_inline void register_bishop_pin(const Position& restrict pos, Square ksq, Square bsq);
-
+	
 	template<Color Us>
 	force_inline void register_rook_pin(const Position& restrict pos, Square ksq, Square rsq);
 
 	template<Color Us>
 	force_inline void register_ep_pin(const Position& restrict pos, Square ksq);
-		
+
 	template<GenType Gt, Color Us, bool EnPassant>
 	force_inline uint64_t king_attacks(const Position& restrict pos);
 
 	template<bool IsCheck>
-	force_inline uint64_t const_checkmask(uint64_t checkmask);
+	force_inline uint64_t const_checkmask() const;
 
-	template<Color C, GenType Gt>
-	force_inline uint64_t moveable_sqs(const Position& restrict pos, uint64_t checkmask);
-} // namespace Movegen
+	template<Color Us, GenType Gt>
+	force_inline uint64_t moveable_sqs(const Position& restrict pos, uint64_t cm) const;
+
+}; // struct GenData
 
 
 template<GenType Gt, Boardstate State, bool IsCheck>
-force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Move* moves)
+force_inline Move* enumerate(const Position& restrict pos, const GenData& restrict gen_data, uint64_t king_atk, Move* moves)
 {
 	constexpr Color Us = State.turn;
 	constexpr Color Them = ~Us;
 
-	const uint64_t rook_pins = Movegen::rook_pins;
-	const uint64_t bishop_pins = Movegen::bishop_pins;
-	const uint64_t checkmask = Movegen::const_checkmask<IsCheck>(Movegen::checkmask);
-	const uint64_t moveable_sqs = Movegen::moveable_sqs<Us, Gt>(pos, checkmask);
+	const uint64_t checkmask = gen_data.const_checkmask<IsCheck>();
+	const uint64_t moveable_sqs = gen_data.moveable_sqs<Us, Gt>(pos, checkmask);
 
 	const Square ksq = pos.king_sq<Us>();
 
@@ -87,18 +85,18 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 			*moves++ = Move(NORMAL, ksq, pop_lsb(king_atk), KING);
 
 		if constexpr (!IsCheck && State.can_castle_queenside())
-			if (State.can_castle_queenside(Movegen::kingban, pos.pieces, pos.bitboard<Us, ROOK>()))
+			if (State.can_castle_queenside(gen_data.kingban, pos.pieces, pos.bitboard<Us, ROOK>()))
 				*moves++ = Move(CASTLING, ksq, ksq + 2 * Direction::W, KING);
 
 		if constexpr (!IsCheck && State.can_castle_kingside())
-			if (State.can_castle_kingside(Movegen::kingban, pos.pieces, pos.bitboard<Us, ROOK>()))
+			if (State.can_castle_kingside(gen_data.kingban, pos.pieces, pos.bitboard<Us, ROOK>()))
 				*moves++ = Move(CASTLING, ksq, ksq + 2 * Direction::E, KING);
 	}
 
 	if constexpr (Gt == LEGAL)
 	{
-		const uint64_t vertical_pawns = pos.bitboard<Us, PAWN>() & ~bishop_pins;
-		const uint64_t diagonal_pawns = pos.bitboard<Us, PAWN>() & ~rook_pins;
+		const uint64_t vertical_pawns = pos.bitboard<Us, PAWN>() & ~gen_data.bishop_pins;
+		const uint64_t diagonal_pawns = pos.bitboard<Us, PAWN>() & ~gen_data.rook_pins;
 
 		uint64_t w_atk_pawns = diagonal_pawns & pawn_atk_east_bb<Them>(pos.bitboard<Them>() & checkmask);
 		uint64_t e_atk_pawns = diagonal_pawns & pawn_atk_west_bb<Them>(pos.bitboard<Them>() & checkmask);
@@ -106,32 +104,32 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 		uint64_t push_pawns = step_pawns & Bitboard::rank_2<Us>() & pawn_push_bb<Them>(~pos.pieces & checkmask);
 		step_pawns &= pawn_step_bb<Them>(checkmask);
 
-		w_atk_pawns &= pawn_atk_east_bb<Them>(bishop_pins) | ~bishop_pins;
-		e_atk_pawns &= pawn_atk_west_bb<Them>(bishop_pins) | ~bishop_pins;
-		step_pawns &= pawn_step_bb<Them>(rook_pins) | ~rook_pins;
-		push_pawns &= pawn_push_bb<Them>(rook_pins) | ~rook_pins;
+		w_atk_pawns &= pawn_atk_east_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
+		e_atk_pawns &= pawn_atk_west_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
+		step_pawns &= pawn_step_bb<Them>(gen_data.rook_pins) | ~gen_data.rook_pins;
+		push_pawns &= pawn_push_bb<Them>(gen_data.rook_pins) | ~gen_data.rook_pins;
 
 		if constexpr (State.has_ep_pawn)
 		{
-			if (Movegen::ep_target != NO_SQUARE)
+			if (gen_data.ep_target != 0)
 			{
-				uint64_t ep_atk_west = diagonal_pawns & shift<E>(checkmask & Movegen::ep_target);
-				uint64_t ep_atk_east = diagonal_pawns & shift<W>(checkmask & Movegen::ep_target);
+				uint64_t ep_atk_west = diagonal_pawns & shift<E>(checkmask & gen_data.ep_target);
+				uint64_t ep_atk_east = diagonal_pawns & shift<W>(checkmask & gen_data.ep_target);
 
 				#if (DEBUG)
-					assert(Bitboard::rank_5<Us>() & Movegen::ep_target);
-					assert(Bitboard::rank_6<Us>() & (Movegen::ep_target + pawn_step<Us>()));
+					assert(Bitboard::rank_5<Us>() & gen_data.ep_target);
+					assert(Bitboard::rank_6<Us>() & (lsb(gen_data.ep_target) + pawn_step<Us>()));
 				#endif
 
 				if (ep_atk_west | ep_atk_east)
 				{
-					ep_atk_west &= pawn_atk_east_bb<Them>(bishop_pins) | ~bishop_pins;
-					ep_atk_east &= pawn_atk_west_bb<Them>(bishop_pins) | ~bishop_pins;
+					ep_atk_west &= pawn_atk_east_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
+					ep_atk_east &= pawn_atk_west_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
 
 					if (ep_atk_west)
-						*moves++ = Move(EN_PASSANT, lsb(ep_atk_west), Movegen::ep_target + pawn_step<Us>(), PAWN);
+						*moves++ = Move(EN_PASSANT, lsb(ep_atk_west), lsb(gen_data.ep_target) + pawn_step<Us>(), PAWN);
 					if (ep_atk_east)
-						*moves++ = Move(EN_PASSANT, lsb(ep_atk_east), Movegen::ep_target + pawn_step<Us>(), PAWN);
+						*moves++ = Move(EN_PASSANT, lsb(ep_atk_east), lsb(gen_data.ep_target) + pawn_step<Us>(), PAWN);
 				}
 			}
 		}
@@ -199,34 +197,34 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 	}
 	else if constexpr (Gt == CAPTURES)
 	{
-		const uint64_t diagonal_pawns = pos.bitboard<Us, PAWN>() & ~rook_pins;
+		const uint64_t diagonal_pawns = pos.bitboard<Us, PAWN>() & ~gen_data.rook_pins;
 
 		uint64_t w_atk_pawns = diagonal_pawns & pawn_atk_east_bb<Them>(pos.bitboard<Them>() & checkmask);
 		uint64_t e_atk_pawns = diagonal_pawns & pawn_atk_west_bb<Them>(pos.bitboard<Them>() & checkmask);
-		w_atk_pawns &= pawn_atk_east_bb<Them>(bishop_pins) | ~bishop_pins;
-		e_atk_pawns &= pawn_atk_west_bb<Them>(bishop_pins) | ~bishop_pins;
+		w_atk_pawns &= pawn_atk_east_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
+		e_atk_pawns &= pawn_atk_west_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
 
 		if constexpr (State.has_ep_pawn)
 		{
-			if (Movegen::ep_target != NO_SQUARE)
+			if (gen_data.ep_target != 0)
 			{
-				uint64_t ep_atk_west = diagonal_pawns & shift<E>(checkmask & Movegen::ep_target);
-				uint64_t ep_atk_east = diagonal_pawns & shift<W>(checkmask & Movegen::ep_target);
+				uint64_t ep_atk_west = diagonal_pawns & shift<E>(checkmask & gen_data.ep_target);
+				uint64_t ep_atk_east = diagonal_pawns & shift<W>(checkmask & gen_data.ep_target);
 
 				#if (DEBUG)
-					assert(Bitboard::rank_5<Us>() & Movegen::ep_target);
-					assert(Bitboard::rank_6<Us>() & (Movegen::ep_target + pawn_step<Us>()));
+					assert(Bitboard::rank_5<Us>() & gen_data.ep_target);
+					assert(Bitboard::rank_6<Us>() & (lsb(gen_data.ep_target) + pawn_step<Us>()));
 				#endif
 
 				if (ep_atk_west | ep_atk_east)
 				{
-					ep_atk_west &= pawn_atk_east_bb<Them>(bishop_pins) | ~bishop_pins;
-					ep_atk_east &= pawn_atk_west_bb<Them>(bishop_pins) | ~bishop_pins;
+					ep_atk_west &= pawn_atk_east_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
+					ep_atk_east &= pawn_atk_west_bb<Them>(gen_data.bishop_pins) | ~gen_data.bishop_pins;
 
 					if (ep_atk_west)
-						*moves++ = Move(EN_PASSANT, lsb(ep_atk_west), Movegen::ep_target + pawn_step<Us>(), PAWN);
+						*moves++ = Move(EN_PASSANT, lsb(ep_atk_west), lsb(gen_data.ep_target) + pawn_step<Us>(), PAWN);
 					if (ep_atk_east)
-						*moves++ = Move(EN_PASSANT, lsb(ep_atk_east), Movegen::ep_target + pawn_step<Us>(), PAWN);
+						*moves++ = Move(EN_PASSANT, lsb(ep_atk_east), lsb(gen_data.ep_target) + pawn_step<Us>(), PAWN);
 				}
 			}
 		}
@@ -271,7 +269,7 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 	}
 
 	{
-		uint64_t knights = pos.bitboard<Us, KNIGHT>() & ~(rook_pins | bishop_pins);
+		uint64_t knights = pos.bitboard<Us, KNIGHT>() & ~(gen_data.rook_pins | gen_data.bishop_pins);
 		while (knights)
 		{
 			const Square from = pop_lsb(knights);
@@ -283,14 +281,14 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 	uint64_t queens = pos.bitboard<Us, QUEEN>();
 
 	{
-		uint64_t bishops = pos.bitboard<Us, BISHOP>() & ~rook_pins;
-		uint64_t pinned_bishops = (bishops | queens) & bishop_pins;
-		uint64_t unpinned_bishops = bishops & ~bishop_pins;
+		uint64_t bishops = pos.bitboard<Us, BISHOP>() & ~gen_data.rook_pins;
+		uint64_t pinned_bishops = (bishops | queens) & gen_data.bishop_pins;
+		uint64_t unpinned_bishops = bishops & ~gen_data.bishop_pins;
 
 		while (pinned_bishops)
 		{
 			const Square from = pop_lsb(pinned_bishops);
-			uint64_t to = BISHOP_MAGICS[from][pos.pieces] & moveable_sqs & bishop_pins;
+			uint64_t to = BISHOP_MAGICS[from][pos.pieces] & moveable_sqs & gen_data.bishop_pins;
 			const PieceType slider = (queens & from ? QUEEN : BISHOP);
 
 			while (to)
@@ -308,14 +306,14 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 	}
 
 	{
-		uint64_t rooks = pos.bitboard<Us, ROOK>() & ~bishop_pins;
-		uint64_t pinned_rooks = (rooks | queens) & rook_pins;
-		uint64_t unpinned_rooks = rooks & ~rook_pins;
+		uint64_t rooks = pos.bitboard<Us, ROOK>() & ~gen_data.bishop_pins;
+		uint64_t pinned_rooks = (rooks | queens) & gen_data.rook_pins;
+		uint64_t unpinned_rooks = rooks & ~gen_data.rook_pins;
 
 		while (pinned_rooks)
 		{
 			const Square from = pop_lsb(pinned_rooks);
-			uint64_t to = ROOK_MAGICS[from][pos.pieces] & moveable_sqs & rook_pins;
+			uint64_t to = ROOK_MAGICS[from][pos.pieces] & moveable_sqs & gen_data.rook_pins;
 			const PieceType slider = (queens & from ? QUEEN : ROOK);
 
 			while (to)
@@ -333,7 +331,7 @@ force_inline Move* enumerate(const Position& restrict pos, uint64_t king_atk, Mo
 	}
 
 	{
-		queens &= ~(rook_pins | bishop_pins);
+		queens &= ~(gen_data.rook_pins | gen_data.bishop_pins);
 		while (queens)
 		{
 			const Square from = pop_lsb(queens);
@@ -352,13 +350,16 @@ inline Move* enumerate(const Position& restrict pos, Move* moves)
 {
 	constexpr Color Us = State.turn;
 
-	uint64_t king_atk = Movegen::king_attacks<Gt, Us, State.has_ep_pawn>(pos);
+	GenData gen_data;
+	gen_data.ep_target = square_to_bb(pos.ep_target);
 
-	if (Movegen::checkmask == Bitboard::BOARD) // not in check
-		moves = enumerate<Gt, State, false>(pos, king_atk, moves);
-	else if (Movegen::checkmask != 0) // single check
-		moves = enumerate<Gt, State, true>(pos, king_atk, moves);
-	else
+	uint64_t king_atk = gen_data.king_attacks<Gt, Us, State.has_ep_pawn>(pos);
+
+	if (gen_data.checkmask == Bitboard::BOARD) // not in check
+		moves = enumerate<Gt, State, false>(pos, gen_data, king_atk, moves);
+	else if (gen_data.checkmask != 0) // single check
+		moves = enumerate<Gt, State, true>(pos, gen_data, king_atk, moves);
+	else // double check, only enumerate king moves
 	{
 		const Square ksq = pos.king_sq<Us>();
 		while (king_atk)
@@ -369,40 +370,40 @@ inline Move* enumerate(const Position& restrict pos, Move* moves)
 }
 
 
-force_inline void Movegen::register_slider_check(Square ksq, Square ssq)
+force_inline void GenData::register_slider_check(Square ksq, Square ssq)
 {
-	if (Movegen::checkmask == Bitboard::BOARD) // no checks yet
-		Movegen::checkmask = BETWEEN_BB[ksq][ssq];
+	if (checkmask == Bitboard::BOARD) // no checks found yet
+		checkmask = BETWEEN_BB[ksq][ssq];
 	else // already one check, hence there is a double check
-		Movegen::checkmask = 0;
+		checkmask = 0;
 
-	Movegen::kingban |= CHECK_RAY_BB[ksq][ssq]; 
+	kingban |= CHECK_RAY_BB[ksq][ssq];
 }
 
 template<Color Us, bool EnPassant>
-force_inline void Movegen::register_bishop_pin(const Position& restrict pos, Square ksq, Square bsq)
+force_inline void GenData::register_bishop_pin(const Position& restrict pos, Square ksq, Square bsq)
 {
 	const uint64_t pinmask = BETWEEN_BB[ksq][bsq];
 
 	if constexpr (EnPassant)
-		if (pinmask & (Movegen::ep_target))
-			Movegen::ep_target = NO_SQUARE;
+		if (pinmask & ep_target)
+			ep_target = 0;
 
 	if (pinmask & pos.bitboard<Us>()) // there lies a friendly piece in the enemy bishop's path
-		Movegen::bishop_pins |= pinmask;
+		bishop_pins |= pinmask;
 }
 
 template<Color Us>
-force_inline void Movegen::register_rook_pin(const Position& restrict pos, Square ksq, Square rsq)
+force_inline void GenData::register_rook_pin(const Position& restrict pos, Square ksq, Square rsq)
 {
 	const uint64_t pinmask = BETWEEN_BB[ksq][rsq];
 
 	if (pinmask & pos.bitboard<Us>()) // there lies a friendly piece in the enemy rook's path
-		Movegen::rook_pins |= pinmask;
+		rook_pins |= pinmask;
 }
 
 template<Color Us>
-force_inline void Movegen::register_ep_pin(const Position& restrict pos, Square ksq)
+force_inline void GenData::register_ep_pin(const Position& restrict pos, Square ksq)
 {
 	constexpr Color Them = ~Us;
 
@@ -411,23 +412,19 @@ force_inline void Movegen::register_ep_pin(const Position& restrict pos, Square 
 
 	if ((Bitboard::rank_5<Us>() & ksq) && (Bitboard::rank_5<Us>() & enemy_rq) && (Bitboard::rank_5<Us>() & pawns))
 	{
-		uint64_t ep_atk_west = pawns & shift<E>(square_to_bb(Movegen::ep_target));
-		uint64_t ep_atk_east = pawns & shift<W>(square_to_bb(Movegen::ep_target));
+		uint64_t ep_atk_west = pawns & shift<E>(ep_target);
+		uint64_t ep_atk_east = pawns & shift<W>(ep_target);
 
-		if ((ep_atk_west && (ROOK_MAGICS[ksq][pos.pieces & ~(ep_atk_west | Movegen::ep_target)] & Bitboard::rank_5<Us>()) & enemy_rq)
-		 || (ep_atk_east && (ROOK_MAGICS[ksq][pos.pieces & ~(ep_atk_east | Movegen::ep_target)] & Bitboard::rank_5<Us>()) & enemy_rq))
-			Movegen::ep_target = NO_SQUARE;
+		if ((ep_atk_west && (ROOK_MAGICS[ksq][pos.pieces & ~(ep_atk_west | ep_target)] & Bitboard::rank_5<Us>()) & enemy_rq)
+		 || (ep_atk_east && (ROOK_MAGICS[ksq][pos.pieces & ~(ep_atk_east | ep_target)] & Bitboard::rank_5<Us>()) & enemy_rq))
+			ep_target = 0;
 	}
 }
 
 template<GenType Gt, Color Us, bool EnPassant>
-force_inline uint64_t Movegen::king_attacks(const Position& restrict pos)
+force_inline uint64_t GenData::king_attacks(const Position& restrict pos)
 {
 	constexpr Color Them = ~Us;
-
-	Movegen::checkmask = Bitboard::BOARD;
-	Movegen::kingban = 0;
-	Movegen::bishop_pins = Movegen::rook_pins = 0;
 
 	const Square ksq = pos.king_sq<Us>();
 	const uint64_t king_bb = pos.bitboard<Us, KING>();
@@ -438,16 +435,16 @@ force_inline uint64_t Movegen::king_attacks(const Position& restrict pos)
 		const uint64_t e_pawn_atk = pawn_atk_east_bb<Them>(pos.bitboard<Them, PAWN>());
 		
 		if (w_pawn_atk & king_bb)
-			Movegen::checkmask = pawn_atk_east_bb<Us>(king_bb);
+			checkmask = pawn_atk_east_bb<Us>(king_bb);
 		else if (e_pawn_atk & king_bb)
-			Movegen::checkmask = pawn_atk_west_bb<Us>(king_bb);
+			checkmask = pawn_atk_west_bb<Us>(king_bb);
 	}
 
 	// knight checks
 	{
 		const uint64_t knight_atk = PIECE_ATTACKS[KNIGHT][ksq] & pos.bitboard<Them, KNIGHT>();
 		if (knight_atk)
-			Movegen::checkmask = knight_atk;
+			checkmask = knight_atk;
 	}
 
 	// bishop checks and pins
@@ -456,11 +453,11 @@ force_inline uint64_t Movegen::king_attacks(const Position& restrict pos)
 		{
 			uint64_t bishop_atk = BISHOP_MAGICS[ksq][pos.pieces] & pos.bitboards<Them, BISHOP, QUEEN>();
 			while (bishop_atk)
-				Movegen::register_slider_check(ksq, pop_lsb(bishop_atk));
+				register_slider_check(ksq, pop_lsb(bishop_atk));
 
 			uint64_t bishop_pin = BISHOP_XRAY_MAGICS[ksq][pos.pieces] & pos.bitboards<Them, BISHOP, QUEEN>();
 			while (bishop_pin)
-				Movegen::register_bishop_pin<Us, EnPassant>(pos, ksq, pop_lsb(bishop_pin));
+				register_bishop_pin<Us, EnPassant>(pos, ksq, pop_lsb(bishop_pin));
 		}
 	}
 
@@ -470,42 +467,42 @@ force_inline uint64_t Movegen::king_attacks(const Position& restrict pos)
 		{
 			uint64_t rook_atk = ROOK_MAGICS[ksq][pos.pieces] & pos.bitboards<Them, ROOK, QUEEN>();
 			while (rook_atk)
-				Movegen::register_slider_check(ksq, pop_lsb(rook_atk));
+				register_slider_check(ksq, pop_lsb(rook_atk));
 
 			uint64_t rook_pin = ROOK_XRAY_MAGICS[ksq][pos.pieces] & pos.bitboards<Them, ROOK, QUEEN>();
 			while (rook_pin)
-				Movegen::register_rook_pin<Us>(pos, ksq, pop_lsb(rook_pin));
+				register_rook_pin<Us>(pos, ksq, pop_lsb(rook_pin));
 		}
 	}
 
 	if constexpr (EnPassant)
 		register_ep_pin<Us>(pos, ksq);
 
-	uint64_t king_moves = PIECE_ATTACKS[KING][ksq] & ~pos.bitboard<Us>() & ~Movegen::kingban;
+	uint64_t king_moves = PIECE_ATTACKS[KING][ksq] & ~pos.bitboard<Us>() & ~kingban;
 	if (king_moves == 0) // the king has no legal moves, no sense in updating kingban any further
 		return 0;
 
 	for (uint64_t enemy_knights = pos.bitboard<Them, KNIGHT>(); enemy_knights; )
-		Movegen::kingban |= PIECE_ATTACKS[KNIGHT][pop_lsb(enemy_knights)];
+		kingban |= PIECE_ATTACKS[KNIGHT][pop_lsb(enemy_knights)];
 
-	Movegen::kingban |= pawn_atk_bb<Them>(pos.bitboard<Them, PAWN>());
+	kingban |= pawn_atk_bb<Them>(pos.bitboard<Them, PAWN>());
 
 	for (uint64_t enemy_bishops = pos.bitboards<Them, BISHOP, QUEEN>(); enemy_bishops; )
-		Movegen::kingban |= BISHOP_MAGICS[pop_lsb(enemy_bishops)][pos.pieces];
+		kingban |= BISHOP_MAGICS[pop_lsb(enemy_bishops)][pos.pieces];
 	
 	for (uint64_t enemy_rooks = pos.bitboards<Them, ROOK, QUEEN>(); enemy_rooks; )
-		Movegen::kingban |= ROOK_MAGICS[pop_lsb(enemy_rooks)][pos.pieces];
+		kingban |= ROOK_MAGICS[pop_lsb(enemy_rooks)][pos.pieces];
 
-	Movegen::kingban |= PIECE_ATTACKS[KING][pos.king_sq<Them>()];
+	kingban |= PIECE_ATTACKS[KING][pos.king_sq<Them>()];
 
 	if constexpr (Gt == CAPTURES)
-		Movegen::kingban |= ~pos.bitboard<Them>();
+		kingban |= ~pos.bitboard<Them>();
 
-	return king_moves & ~Movegen::kingban;
+	return king_moves & ~kingban;
 }
 
 template<bool IsCheck>
-force_inline uint64_t Movegen::const_checkmask(uint64_t checkmask)
+force_inline uint64_t GenData::const_checkmask() const
 {
 	if constexpr (IsCheck)
 		return checkmask;
@@ -513,22 +510,21 @@ force_inline uint64_t Movegen::const_checkmask(uint64_t checkmask)
 		return Bitboard::BOARD;
 }
 
-template<Color C, GenType Gt>
-force_inline uint64_t Movegen::moveable_sqs(const Position& restrict pos, uint64_t checkmask)
+template<Color Us, GenType Gt>
+force_inline uint64_t GenData::moveable_sqs(const Position& restrict pos, uint64_t cm) const
 {
-	static_assert(Gt == LEGAL || Gt == CAPTURES);
 	if constexpr (Gt == LEGAL)
-		return ~pos.bitboard<C>() & checkmask;
+		return ~pos.bitboard<Us>() & cm;
 	if constexpr (Gt == CAPTURES)
-		return pos.bitboard<~C>() & checkmask;
+		return pos.bitboard<~Us>() & cm;
+	else
+		return 0;
 }
-
 
 
 template<GenType Gt>
 force_inline Move* enumerate(const Position& restrict pos, Move* moves)
 {
-	Movegen::ep_target = pos.ep_target;
 	switch (pos.boardstate_pattern())
 	{
 		case 0:  return enumerate<Gt, 0>(pos, moves);
